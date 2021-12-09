7be57f4ddc1b488c1c14b7fe323eb6f3cc504e27
Alireza <alirezamolaii@gmail.com>
2021-12-09 14:14:39 +0330

    init

---
+++ main.js
@@ -0,0 +1,18 @@
+const startPoint = {
+  x: 100,
+  y: 200,
+};
+
+const endPoint = {
+  x: 300,
+  y: 400,
+};
+
+drawLine(startPoint, endPoint);
+
+const centerPoint = {
+  x: 400,
+  y: 200,
+};
+
+drawCircle(centerPoint, 100);
